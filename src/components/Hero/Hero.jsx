98f9c56354fca6c7f44244771ff09120d78df5f2
Ashutosh8668 <a.rajput86684@gmail.com>
2024-02-02 00:33:40 +0530

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Ashutosh Rajput</h1>
+        <p className={styles.description}>
+          I'm a full-stack developer with  experience  of using React and
+          Springboot.I am a undergraduate student of BTech from Computer Engineering branch.
+        </p>
+        <a href="ashutosh.rajput@mitaoe.ac.in" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/hero.jpeg")}
+        alt="My profile image of myself"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
