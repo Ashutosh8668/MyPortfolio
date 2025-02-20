05640b0273b431fa05af063d1c20422caac8fe69
Ashutosh Rajput <121126646+Ashutosh8668@users.noreply.github.com>
2025-02-20 22:27:05 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:ashutosh.rajput@mitaoe.ac.in">ashutosh.rajput@mitaoe.ac.in</a>
+          <a href="mailto:ashutosh.rajput@mitaoe.ac.in">a.rajput866843@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
