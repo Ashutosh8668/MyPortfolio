09a119cae52f050e774ca63c02ed192aea824bcb
Ashutosh Rajput <121126646+Ashutosh8668@users.noreply.github.com>
2025-04-19 00:25:53 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -27,11 +27,11 @@ export const Contact = () => {
           <a href="https://github.com/Ashutosh8668">https://github.com/Ashutosh8668</a>
         </li>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/codechef.png")} alt="Github icon" />
+          <img src={getImageUrl("contact/codechef.png")} alt="Codechef icon" />
           <a href="https://www.codechef.com/users/ashx9">https://www.codechef.com/users/ashx9</a>
         </li>
          <li className={styles.link}>
-          <img src={getImageUrl("contact/leetcode.png")} alt="Github icon" />
+          <img src={getImageUrl("contact/leetcode.png")} alt="Leetcode icon" />
           <a href="https://leetcode.com/u/AVRaj45/">https://leetcode.com/u/AVRaj45/</a>
         </li>
       </ul>
